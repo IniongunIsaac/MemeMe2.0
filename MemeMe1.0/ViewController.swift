d09e7cfd64b631357cfe636578fb69ebe2683278
Isaac Iniongun <iniongunisaac@gmail.com>
2019-12-05 11:43:11 +0100

    Initial Commit

---
+++ MemeMe1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe1.0
+//
+//  Created by Isaac Iniongun on 05/12/2019.
+//  Copyright © 2019 Ing Groups. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
